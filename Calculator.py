984eb681ddcfe99c824950981d68c6530ccfbe0f
v.bliznyakov <genis20041210@gmail.com>
2025-11-11 11:53:46 +0300

    first

---
+++ Calculator.py
@@ -0,0 +1,81 @@
+def precedence(op):
+    if op in ('+', '-'):
+        return 1
+    if op in ('*', '/'):
+        return 2
+    if op == '^':
+        return 3
+    return 0
+
+
+def to_rpn(expression):
+    output = []
+    stack = []
+    i = 0
+    while i < len(expression):
+        token = expression[i]
+
+        if token.isdigit() or (token == '.' and i + 1 < len(expression) and expression[i + 1].isdigit()):
+            num = token
+            while i + 1 < len(expression) and (expression[i + 1].isdigit() or expression[i + 1] == '.'):
+                i += 1
+                num += expression[i]
+            output.append(num)
+
+        elif token == '(':
+            stack.append(token)
+
+        elif token == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+
+        else:
+            while stack and precedence(stack[-1]) >= precedence(token):
+                output.append(stack.pop())
+            stack.append(token)
+
+        i += 1
+
+    while stack:
+        output.append(stack.pop())
+
+    return output
+
+
+def eval_rpn(rpn):
+    stack = []
+    for token in rpn:
+        if token not in '+-*/^':
+            stack.append(float(token))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(a / b)
+            elif token == '^':
+                stack.append(a ** b)
+    return stack[0]
+
+
+def calculate(expression):
+    expression = expression.replace(' ', '')
+    rpn = to_rpn(expression)
+    print(rpn)
+    result = eval_rpn(rpn)
+    return result
+
+
+if __name__ == "__main__":
+    expr = input("Введите выражение: ")
+    try:
+        res = calculate(expr)
+        print("Результат:", res)
+    except Exception as e:
+        print("Ошибка в выражении:", e)
---
+++ test.py
@@ -0,0 +1,35 @@
+import unittest
+import Calculator as calc
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_precedence(self):
+        self.assertEqual(calc.precedence('+'), 1)
+        self.assertEqual(calc.precedence('-'), 1)
+        self.assertEqual(calc.precedence('*'), 2)
+        self.assertEqual(calc.precedence('/'), 2)
+        self.assertEqual(calc.precedence('^'), 3)
+        self.assertEqual(calc.precedence('('), 0)
+        self.assertEqual(calc.precedence('x'), 0)
+
+    def test_to_rpn_simple(self):
+        self.assertEqual(calc.to_rpn("3+4*2/(1-5)^2^3"),
+                         ['3', '4', '2', '*', '1', '5', '-', '2', '^', '3', '^', '/', '+'])
+        self.assertEqual(calc.to_rpn("1+(2*3)"), ['1', '2', '3', '*', '+'])
+        self.assertEqual(calc.to_rpn("(1+2)*3"), ['1', '2', '+', '3', '*'])
+
+    def test_eval_rpn_basic(self):
+        rpn = ['3', '4', '2', '*', '+']
+        self.assertAlmostEqual(calc.eval_rpn(rpn), 11.0)
+        rpn2 = ['5', '1', '2', '+', '4', '*', '+', '3', '-']
+        self.assertAlmostEqual(calc.eval_rpn(rpn2), 14.0)
+
+    def test_calculate_integration(self):
+        self.assertAlmostEqual(calc.calculate("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"), 3.001953125)
+        self.assertAlmostEqual(calc.calculate("1 + (2 * 3)"), 7)
+        self.assertAlmostEqual(calc.calculate("(1 + 2) * 3"), 9)
+
+
+if __name__ == "__main__":
+    unittest.main()
